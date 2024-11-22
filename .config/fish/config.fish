231e15caa3759da95f54c32a0ee130ab53f46ecb
eXsoR65 <xrodriguez65@gmail.com>
2024-11-22 15:41:27 -0500

    Updated dotfiles and README

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,90 @@
+# 
+# eXsoR 
+# GitHub: https://github.com/exsor65
+#
+# My Fish Config.
+
+### INTERACTIVE ###
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  atuin init fish | source
 end
 
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.bin $HOME/.local/bin /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths
+
+### EXPORT ###
+set fish_greeting
+set EDITOR "nvim"
+
+### "nvim" as manpager
+set -x MANPAGER "nvim +Man!"
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+### FUNCTIONS ###
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+### END OF FUNCTIONS ###
+
+## ALIASES ###
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# vim and emacs
+alias vim='nvim'
+
+# Changing "ls" to "eza"
+alias ls='eza -la --color=always --group-directories-first' # my preferred listing
+alias la='eza -a --color=always --group-directories-first'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first'  # long format
+alias lt='eza -lah --sort age' # sort by age
+
+# adding flags
+alias df='df -h'               # human-readable sizes
+alias grep='grep --color=auto' # colorize output (good for log files)
+
+### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
+
+### SETUP ZOXIDE ###
+zoxide init fish | source
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Dotfiles
+
+My dotfiles are for the following: 
+ - Fish Shell
+ - Starship
+ - Hyprland
+ - Kitty
+ - NeoVIM
+ - Waybar
